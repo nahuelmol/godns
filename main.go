99e34ff4c383e9db1700b4d68bc2b5f371c738a0
nahuelmol <kindermolina98@gmail.com>
2024-12-06 23:10:53 -0300

    idk;

--- main.go
+++ main.go
@@ -4,32 +4,29 @@ import (
     "fmt"
     "log"
     "net"
-
     "dnsservice/node/nodes"
 )
 
 func main(){
     udpaddr, err := net.ResolveUDPAddr("udp", ":8080")
     if err != nil {
-        fmt.Printf("err resolving address")
+        fmt.Printf("err resolving udp:8080 address")
         return
     }
-
     udpconn, err := net.ListenUDP("udp", udpaddr)
     if err != nil {
-        fmt.Printf("error in listener %s", err)
+        fmt.Printf("error listeninig %s", err)
     }
     defer udpconn.Close()
-
     log.Println("UDP is listening", udpaddr)
-
     buffer := make([]byte, 1024)
     for {
         n, addr, err := udpconn.ReadFromUDP(buffer)
         if err != nil {
-            fmt.Println("error reading UDP incomming messagas", err)
+            fmt.Println("error reading UDP buffer", err)
+            fmt.Println("buffer size: %d", n)
         }
-        fmt.Printf("\nrequest from: %s\n", addr)
+        fmt.Printf("\nrequest from: %s", addr)
         nodes.AnalizeQuery(buffer[:n])
     }
 }
--- nodes/query.go
+++ nodes/query.go
@@ -3,20 +3,22 @@ package nodes
 import (
     "fmt"
     "encoding/binary"
-
-    "dnsservice/node/utils"
+    "errors"
+    //"dnsservice/node/utils"
 )
 
 type Query struct {
-    MAChead     uint16
-    IPv         uint16
-    UDPheader   uint16
+    IP  [20]byte
+    MAC [14]byte
+    UDP [8]byte
+
+    UDPheader   uint16 // this is 8 bytes
 
     checksum    uint16
     
     //dns things
-    trans_id    uint16
-    flags       string
+    //trans_id    uint16
+    //flags       string
 
     questions   uint16
     ansRR       uint16
@@ -31,44 +33,52 @@ type Query struct {
 
 func newQuery(message []byte) *Query {
 
+    //mac := message[:14]
+    //ip  := message[14:35]
+    udp := message[35:43]
+
+    length  := udp[5:6]
+    byte1   := length[0]
+    byte2   := length[1]
+    size    := uint16(byte1)<<8 | uint16(byte2)
+
+    otherUDPsize := binary.BigEndian.Uint16(udp[5:7]) // from 5 to 6
+    fmt.Printf("size %d", size)
+    fmt.Print("size %d", otherUDPsize)
+
     var message_n = len(message)
     fmt.Printf("message lenght:%d\n", message_n)
 
     available_classes := [...]string{"INET"}
     fmt.Printf("%s\n",available_classes[0])
 
-    MAChead := binary.BigEndian.Uint16(message[:14])
+    //MAChead := binary.BigEndian.Uint16(message[:14])
 
-    ipHeader := 14
-    ipVersion := "unknown IPv"
-    ipLength := 20
+    //ipHeader := 14
+    //ipVersion := "unknown IPv"
+    //ipLength := 20
 
-    if message[ipHeader]&0xF0 == 0x40 {
+    /*if message[ipHeader]&0xF0 == 0x40 {
         ipVersion = "IPv4"
         ipLength = 20
     } else if message[ipHeader]&0xF0 == 0x60 {
         ipVersion = "IPv6"
         ipLength = 40
-    }
+    }*/
+    //fmt.Printf("the ip version is: %s\n", ipVersion)
+    //ipEnd := 14 + ipLength
+    //IPv    := binary.BigEndian.Uint16(message[14:ipEnd])
 
-    fmt.Printf("the ip version is: %s\n", ipVersion)
+    //UDPstt := ipEnd
+    //UDPend := UDPstt + 8
+    //UDPheader := binary.BigEndian.Uint16(message[UDPstt:UDPend])
+    //DNSstt := UDPend
+    //chksum := DNSstt + 10
 
+    checksum := uint16(binary.BigEndian.Uint16(message[0:2]))
 
-    ipEnd := 14 + ipLength
-    IPv    := binary.BigEndian.Uint16(message[14:ipEnd])
-
-    UDPstt := ipEnd
-    UDPend := UDPstt + 8
-
-    UDPheader := binary.BigEndian.Uint16(message[UDPstt:UDPend])
-
-    DNSstt := UDPend
-    chksum := DNSstt + 10
-
-    checksum := binary.BigEndian.Uint16(message[DNSstt:chksum])
-
-    trans_id   := binary.BigEndian.Uint16(message[10:12])
-    flags      := utils.TakeFlags(message[12:14])
+    //trans_id   := binary.BigEndian.Uint16(message[10:12])
+    //flags      := utils.TakeFlags(message[12:14])
 
     questions:= binary.BigEndian.Uint16(message[14:16])
     ansRR   := binary.BigEndian.Uint16(message[16:18])
@@ -81,26 +91,17 @@ func newQuery(message []byte) *Query {
     //here I must convert byte data into string or legible data in general
 
     return &Query {
-
-        MAChead,
-        IPv,
-        UDPheader, 
-
-        checksum,
+        checksum:checksum,
             
-        //dns things
-        trans_id,
-        flags,
-
-        questions,
-        ansRR,
-        authRR,
-        addRR,
+        questions:questions,
+        ansRR:ansRR,
+        authRR:authRR,
+        addRR:addRR,
 
         //the query
-        domain,
-        queryType,
-        queryClass,
+        domain:domain,
+        queryType:queryType,
+        queryClass:queryClass,
     }
 }
 
@@ -108,16 +109,16 @@ func newQuery(message []byte) *Query {
 func AnalizeQuery(message []byte) {
         
     query := newQuery(message);
-    fmt.Printf("IPv4:%d\n ", query.IPv)
-    fmt.Printf("Mac Head:%d\n ", query.MAChead)
-    fmt.Printf("UDP head:%d\n ", query.UDPheader)
+    //fmt.Printf("IPv4:%d\n ", query.IPv)
+    //fmt.Printf("Mac Head:%d\n ", query.MAChead)
+    //fmt.Printf("UDP head:%d\n ", query.UDPheader)
 
     fmt.Printf("questions:%d\n ", query.questions)
     fmt.Printf("answer:%d\n ", query.ansRR)
     fmt.Printf("auth:%d\n ", query.authRR)
     fmt.Printf("add:%d\n ", query.addRR)
 
-    fmt.Printf("flags:%s\n", query.flags)
+    //fmt.Printf("flags:%s\n", query.flags)
     fmt.Printf("domain:%s\n", query.domain)
     fmt.Printf("query type:%s\n", query.queryType)
     fmt.Printf("query class:%s\n", query.queryClass)
@@ -126,19 +127,23 @@ func AnalizeQuery(message []byte) {
     //counter returns a referral to other authorative server
 
     //SendReferral(domain)
+    //I need to know how is the structure of a query
 }
 
 func SendReferral(domain string) string {
-    myTLD := GetTLD(domain)
+    tld, err := GetTLD(domain)
+    if err != nil {
+        fmt.Println("err: %s", err)
+    }
     fmt.Printf("sending the referral...")
 
-    referral := CreateReferral(14, domain, myTLD)
+    referral := CreateReferral(14, domain, tld)
     fmt.Println(referral)
-    //I must research about anamtomy of a reerral query
+    //I must research about anamtomy of a referal query
     return "referral"
 }
 
-func GetTLD(domain string) string {
+func GetTLD(domain string) (string, error) {
     var TLDs = []string{"com", "net", "edu", "org"}
     var TLD string
     var aux string
@@ -151,13 +156,10 @@ func GetTLD(domain string) string {
         aux += TLD
         TLD = aux
     }
-
-    for _, tld := range TLDs{
+    for _, tld := range TLDs {
         if tld == TLD {
-            return TLD
+            return TLD, nil
         }
     }
-    return TLD
+    return "", errors.New("the domain is not recognized")
 }
-
-
